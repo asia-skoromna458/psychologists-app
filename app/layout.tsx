61c18f537ddab9eddc962a21d4fc0dbe0489c2b5
asia <asia.skoromna@gmail.com>
2026-08-19 21:37:49 +0300

    init

---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "MindSpace",
+  description: "App for psychologists services",
+};
+
+export default function RootLayout({ children }: LayoutProps<"/">) {
+  return (
+    <html lang="en" className={`${inter.variable} `}>
+      <body>{children}</body>
+    </html>
+  );
+}
