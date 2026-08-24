114b5768bcc354f953c42cabb0bd96447eebac9a
asia <asia.skoromna@gmail.com>
2026-08-24 23:47:36 +0300

    feat: add active state logic to header navigation

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,7 +1,10 @@
+"use client";
 import Link from "next/link";
 import css from "./Header.module.css";
+import { usePathname } from "next/navigation";
 
 export default function Header() {
+  const pathname = usePathname();
   return (
     <header className={css.header}>
       <div className={css.container}>
@@ -9,8 +12,18 @@ export default function Header() {
           <span className={css.logoSpan}>psychologists.</span>services
         </Link>
         <nav className={css.navigation}>
-          <Link href="/">Home</Link>
-          <Link href="/psychologists">Psychologists</Link>
+          <Link
+            href="/"
+            className={`${css.navLink} ${pathname === "/" ? css.active : ""}`}
+          >
+            Home
+          </Link>
+          <Link
+            href="/psychologists"
+            className={`${css.navLink} ${pathname === "/psychologists" ? css.active : ""}`}
+          >
+            Psychologists
+          </Link>
           <Link href="/favorites" className={css.favorites}>
             Favorites
           </Link>
