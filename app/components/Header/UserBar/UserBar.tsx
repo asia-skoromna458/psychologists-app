17c865e73f52ab5eaf27b40f7d474dcb1f5e8d68
asia <asia.skoromna@gmail.com>
2026-09-11 23:56:52 +0300

    style: update user icon

--- app/components/Header/UserBar/UserBar.tsx
+++ app/components/Header/UserBar/UserBar.tsx
@@ -1,14 +1,16 @@
-import { RiFileUserFill } from "react-icons/ri";
 import css from "./UserBar.module.css";
 import { logout, useAuthStore } from "@/lib/firebase/auth";
 import { useFavoriteStore } from "@/lib/store/favorite";
+import Image from "next/image";
 
 export default function UserBar() {
   const favorites = useFavoriteStore((state) => state.setFavorites);
   const user = useAuthStore((state) => state.user);
   return (
     <div className={css.container}>
-      <RiFileUserFill className={css.icon} />
+      <div className={css.iconWrapper}>
+        <Image src="/user_icon.svg" width={16} height={16} alt="User_icon" />
+      </div>
       <p className={css.name}>{user?.displayName}</p>
       <button
         className={css.logOutBtn}
